bc2913c047a9593274ed0bab5d22a9abec5fc8ba
Mohit Kumar Suman <m123aryan123@gmail.com>
2024-05-01 09:38:48 +0530

    Going to create backend functionality for order

---
+++ server/src/routes/order.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import { newOrder } from '../controllers/order.js';
+import { adminOnly } from '../middlewares/auth.js';
+
+const app = express.Router();
+
+app.post("/new",adminOnly, newOrder);
