aec73c81bba7c4db4fa8b8af114a8e5adc5f7220
Mohit Kumar Suman <m123aryan123@gmail.com>
2024-05-05 17:30:36 +0530

    created types for order

--- server/dist/controllers/order.js
+++ server/dist/controllers/order.js
@@ -1,4 +1,3 @@
 import { TryCatch } from "../middlewares/error.js";
 export const newOrder = TryCatch(async (req, res, next) => {
-    console.log("");
 });
--- server/src/controllers/order.ts
+++ server/src/controllers/order.ts
@@ -1,5 +1,9 @@
+import { Request } from "express";
 import { TryCatch } from "../middlewares/error.js";
+import { NewOrderRequestBody } from "../types/types.js";
 
-export const newOrder = TryCatch(async(req, res, next) => {
-   console.log("")
-});
+export const newOrder = TryCatch(
+  async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
+    
+  }
+);
