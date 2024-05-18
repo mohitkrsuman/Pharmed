56ef74dc10023e47174b70ff4b1b77b7df5e1d7e
Mohit Kumar Suman <m123aryan123@gmail.com>
2024-05-18 17:53:51 +0530

    creating routes to get admin dashboard stats

---
+++ server/src/controllers/stats.ts
@@ -0,0 +1,44 @@
+import { myCache } from "../app.js";
+import { TryCatch } from "../middlewares/error.js";
+import { Product } from "../models/product.js";
+
+export const getDashboardStats = TryCatch(async (req, res, next) => {
+  let stats = {};
+
+  if (myCache.has("admin-stats")) {
+    stats = JSON.parse(myCache.get("admin-stats") as string);
+  } else {
+    const today = new Date();
+    const startOfThisMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    const endOfThisMonth = today;
+    let startOfLastMonth, endOfLastMonth;
+    if (today.getMonth() === 1) {
+      startOfLastMonth = new Date(today.getFullYear() - 1, 12, 1);
+      endOfLastMonth = new Date(today.getFullYear(), 12, 0);
+    } else {
+      startOfLastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+      endOfLastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    }
+
+    const thisMonthProduct = await Product.find({
+      createdAt:{
+         $gte: startOfThisMonth,
+         $lte: endOfThisMonth
+      }
+    });
+
+   //  console.log(endOfLastMonth.toDateString());
+  }
+
+
+  return res.status(200).json({
+    success: true,
+    stats,
+  });
+});
+
+export const getPieCharts = TryCatch(async (req, res, next) => {});
+
+export const getBarCharts = TryCatch(async (req, res, next) => {});
+
+export const getLineCharts = TryCatch(async (req, res, next) => {});
