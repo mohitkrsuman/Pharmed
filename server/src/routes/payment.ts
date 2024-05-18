c8dff8a4f04c5d45a1a8b008bf20a56b30a9a893
Mohit Kumar Suman <m123aryan123@gmail.com>
2024-05-18 16:42:37 +0530

    added all coupon routes

--- server/src/routes/payment.ts
+++ server/src/routes/payment.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { adminOnly } from '../middlewares/auth.js';
-import { applyDiscount, newCoupon } from '../controllers/payment.js';
+import { applyDiscount, getAllCoupons, newCoupon } from '../controllers/payment.js';
 
 const app = express.Router();
 
@@ -9,6 +9,10 @@ app.post("/coupon/new", newCoupon);
 
 // Route -/api/v1/payment/coupon/new
 app.get("/discount", applyDiscount);
+
+// Route -/api/v1/payment/coupon/all
+app.get("allCoupons", getAllCoupons);
+
 
 
 export default app;
